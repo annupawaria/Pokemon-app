203732465a65bd5de4beb154c97db5504dcc1929
annu <annupawadia@gmail.com>
2024-08-30 16:27:42 +0530

    adding some basic things

--- my-react-app/src/components/PokemonList1.js
+++ my-react-app/src/components/PokemonList1.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const PokemonList1 = () => {
+    const [searchTerm,setSearchTerm]=useState("")
+useEffect(()=>{
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then(response=>{
+
+    }
+
+    )
+})
   return (
-    <div>PokemonList1</div>
+    <div>
+        <div>
+            <input type='text'
+            placeholder='search pokemon'
+            value={searchTerm}
+            onChange={e=>setSearchTerm(e.target.value)}/>
+        </div>
+    </div>
   )
 }
 
